c3b468f19d2ca93ba9d30c80c1921e4b58bfaab9
katechhka <kalininak03@gmail.com>
2022-11-21 22:14:07 +0300

    меню мобилка

---
+++ assets/script/menu.js
@@ -0,0 +1,9 @@
+function openNav() {
+    
+    document.getElementById("ul").style.width = "280px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("ul").style.width = "0";
+}
