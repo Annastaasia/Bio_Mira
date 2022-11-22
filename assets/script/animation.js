93155a6b14d753bf3a2a1db04f495f4fdbee7f41
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2022-11-23 00:33:08 +0300

    Пробы пера

--- assets/script/animation.js
+++ assets/script/animation.js
@@ -1,8 +1,41 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
     $("#footer, #header, #mainpage").on("click", "a", function (event) {
         event.preventDefault();
         let id = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({ scrollTop: top }, 3000);
+        $('body,html').animate({ scrollTop: top }, 1400);
     });
-});
+});*/
+
+function scrollToHalf() {
+
+    $('body, html').animate( {
+            scrollTop: $('#otzivy').offset().top
+        }, 500);
+};
+
+/*function scrollToHal(){
+    var height = $('body').height();
+      $('html, body').animate({
+           scrollTop: height
+      }, 500);
+    };*/
+
+
+    let btn_home = document.getElementById('btn-home');
+    let home = document.getElementById('home');
+    let summary = document.getElementById('summary');
+    
+    function scrollTo(element) {
+      window.scroll({
+        left: 0,
+        top: element.offsetTop,
+        behavior: 'smooth'
+      })
+    }
+
+
+    btn_home.addEventListener('click', (e) => {
+        e.preventDefault()
+          scrollTo(summary)
+        })
