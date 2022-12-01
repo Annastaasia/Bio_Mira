a9731b4654350f4e67b33bc5474a3f7a88745fba
katechhka <kalininak03@gmail.com>
2022-12-01 14:34:59 +0300

    анимация

--- assets/script/animation.js
+++ assets/script/animation.js
@@ -1,41 +1,8 @@
-$(document).ready(function () {
-    $("#footer, #header, #mainpage").on("click", "a", function (event) {
-        event.preventDefault();
-        let id = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({ scrollTop: top }, 1400);
-    });
+$(document).ready(function(){
+  $("#footer, #header, #mainpage").on("click","a", function (event) {
+  event.preventDefault();
+  var id = $(this).attr('href'),
+  top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top}, 1500);
+  });
 });
-
-/*function scrollToHalf() {
-
-    $('body, html').animate( {
-            scrollTop: $('#otzivy').offset().top
-        }, 500);
-};*/
-
-/*function scrollToHal(){
-    var height = $('body').height();
-      $('html, body').animate({
-           scrollTop: height
-      }, 500);
-    };*/
-
-
-    /*let btn_home = document.getElementById('btn-home');
-    let home = document.getElementById('home');
-    let summary = document.getElementById('summary');
-    
-    function scrollTo(element) {
-      window.scroll({
-        left: 0,
-        top: element.offsetTop,
-        behavior: 'smooth'
-      })
-    }
-
-
-    btn_home.addEventListener('click', (e) => {
-        e.preventDefault()
-          scrollTo(summary)
-        })*/
